66c88c28acc03744bd27e6056c6cae30f29c3a7f
Valerii3 <vallerus55@gmail.com>
2022-10-12 23:49:16 +0200

    :(

--- src/Person.hs
+++ src/Person.hs
@@ -2,9 +2,15 @@ module Person where
 
 import qualified Data.Set as Set
 
-data Tree = ???
+data Tree a = Tree 
+  { person :: Person
+  , parent1 :: Maybe (Tree Person)
+  , parent2 :: Maybe (Tree Person)
+  , children :: [Person]
+  }
+  deriving (Show, Eq, Ord)
 
-data Document = ???
+
 
 -- Тип данных для человека
 data Person = Person
@@ -12,14 +18,22 @@ data Person = Person
   , lastName :: String          -- Фамилия, должна быть непустой
   , formerLastNames :: [String] -- Предыдущие фамилии, если фамилия менялась
   , age :: Int                  -- Возраст, должен быть неотрицательным
-  , idNumber :: Maybe Document  -- Какое-то удостоверение личности
-  , parents :: ??? Person       -- Родители данного человека. Выбрать подходящий контейнер.
+  , certificateId :: Int        -- Какое-то удостоверение личности
+  , passportId :: (Int, Int)
+  , parents :: (Maybe Person, Maybe Person)       -- Родители данного человека. Выбрать подходящий контейнер.
   }
-  deriving (Show, Eq)
+  deriving (Show, Eq, Ord)
 
 -- Создание ребенка данных родителей
-createChild :: ??? Person -> Person
-createChild = undefined
+createChild :: Maybe Person -> Maybe Person -> String ->  Int -> Person -- Родитель 1, Родитель 2, Имя, Фамилия, Документ
+createChild (Just parent1) (Just parent2) child_name document_id =
+  Person { firstName = child_name
+  , lastName = lastName parent1
+  , formerLastNames = []
+  , age = 0
+  , certificateId = document_id
+  , passportId = (0, 0)
+  , parents = (Just parent1, Just parent2) }
 
 -- Самый далекий предок данного человека.
 -- Если на одном уровне иерархии больше одного предка -- вывести самого старшего из них.
--- test/Test/Person.hs
+++ test/Test/Person.hs
@@ -2,3 +2,39 @@ module Test.Person where
 
 import Test.Tasty.HUnit (Assertion, (@?=), assertBool)
 import Person
+import qualified Data.Set as Set
+
+person1 :: Person
+person1 =
+  Person { firstName = "Name1"
+         , lastName = "lastname1"
+         , formerLastNames = []
+         , age = 27
+         , certificateId = 0
+         , passportId = (1111, 222222)
+         , parents = (Nothing, Nothing) }
+
+person2 :: Person
+person2 =
+  Person { firstName = "Name2"
+         , lastName = "lastname2"
+         , formerLastNames = []
+         , age = 32
+         , certificateId = 0
+         , passportId = (1234, 567820)
+         , parents = (Nothing, Nothing) }
+
+
+child1 :: Person
+child1 =
+  Person { firstName = "child"
+         , lastName = "lastname2"
+         , formerLastNames = []
+         , age = 0
+         , certificateId = 1
+         , passportId = (0, 0)
+         , parents = (Just person2, Just person1)}
+
+
+unit_createChild = do
+  createChild (Just person2, Just person1, "child", 1) @?= child1
